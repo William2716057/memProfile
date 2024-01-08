1aac28287f92dc459b92a18e512c496413dbafaa
William Spriggs <Willspri@gmail.com>
2024-01-08 13:22:04 +1300

    Add project files.

---
+++ memProfile/memProfile.py
@@ -0,0 +1,10 @@
+from memory_profiler import profile, memory_usage
+
+def myFunction(listSize):
+    list1 = ['word1'] * listSize
+    list2 = ['word2'] * listSize
+    del list2
+    return list1
+
+myFunction(1000000)
+
